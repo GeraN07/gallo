6ba621a3a892c50d3239121e44678b7369aaa0ab
GeraN07 <gera3050@yandex.ru>
2021-04-01 14:39:15 +0300

    slide updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 require("../node_modules/slick-carousel/slick/slick.min.js")
 require("../src/pages/main.js")
 $(document).ready(function(){
-    $('h2').each(function(){
-     var title = $(this);
-     title.html( title.text().replace(/(^\w+)/,'<span>$1</span>') );
+    $('.slide-card__image-title').each(function() {
+        var h = $(this).html();
+        var index = h.indexOf(' ');
+        if(index == -1) {
+            index = h.length;
+        }
+        $(this).html('<span>' + h.substring(0, index) + '</span>' + h.substring(index, h.length));
     });
-   });
+        $('.slide-second-card__image-title').each(function() {
+            var h = $(this).html();
+            var index = h.indexOf(' ');
+            if(index == -1) {
+                index = h.length;
+            }
+            $(this).html('<span class="white-span">' + h.substring(0, index) + h.substring(9,index) + '</span>' + "<br>" + h.substring(9, h.length));
+        });
+    
+});
